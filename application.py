49abb36147208185636569ab006609c1e3a0e9e8
Shrijit Singh <shrijitsingh99@gmail.com>
2018-10-16 02:29:59 +0530

    Fix bug causing server to fail to run on azure

--- application.py
+++ application.py
@@ -1,3 +1,11 @@
+from os import environ
 from SOSial import app
 
-app.run(host='0.0.0.0', port=8080, debug=True)
+
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
