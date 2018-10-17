a6592a9c2f43b424b1b644e6026caf4c4f6bb7dc
Shrijit Singh <shrijitsingh99@gmail.com>
2018-10-17 18:00:27 +0530

    Update localhost default address

--- application.py
+++ application.py
@@ -4,8 +4,10 @@ from SOSial import app, db
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = "0.0.0.0"
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        # PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = 5000
     except ValueError:
         PORT = 5555
 
